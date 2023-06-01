0f9408bdd255e53f98de8981d88376a59f8df544
Scott McIntire <ldg@users.noreply.github.com>
2023-06-01 18:33:16 -0400

    added js to toggle submenu links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,12 +5,18 @@ const closeIcon = document.querySelector('.hero__logo--container .hero--close');
 const mobileIconsContainer = document.querySelector('.hero__mobileicons--container');
 const heroNav = document.querySelector('.hero__nav');
 const desktopMenuLinks = document.querySelectorAll('ul.menu--desktop.menu-left li.menu__item--desktop');
+const mProduct = document.getElementById('mobile-product');
+const mCompany = document.getElementById('mobile-company');
+const mConnect = document.getElementById('mobile-connect');
 let width = window.matchMedia("(min-width: 600px)");
 
 /* event listeners */
 hamburgerIcon.addEventListener('click', toggleMenuIcon);
 closeIcon.addEventListener('click', toggleMenuIcon);
 mobileIconsContainer.addEventListener('click', openMobileNav);
+mProduct.addEventListener('click', openMobileSubmenu );
+mCompany.addEventListener('click', openMobileSubmenu );
+mConnect.addEventListener('click', openMobileSubmenu );
 // - https://fjolt.com/article/javascript-multiple-elements-addeventlistener
 // Array.prototype.forEach.call(desktopMenuLinks, function(item) {
 //     item.addEventListener('click', openDesktopMenu );
@@ -19,6 +25,17 @@ mobileIconsContainer.addEventListener('click', openMobileNav);
 
 
 /* functions */
+function openMobileSubmenu(e) {
+    // console.log(e.target);
+    let submenuLinkTarget = this.querySelector('.submenu');
+    if(submenuLinkTarget.classList.contains('submenu-open')) {
+        submenuLinkTarget.classList.remove('submenu-open');
+    } else {
+        submenuLinkTarget.classList.add('submenu-open');
+    }
+
+}
+
 function openDesktopMenu(e) {
     let linkTarget = this.querySelector('.submenu--desktop');
     if(linkTarget.classList.contains('open') ){
@@ -26,8 +43,6 @@ function openDesktopMenu(e) {
     } else {
         linkTarget.classList.add('open');
     }
-    
-    // console.log(this.querySelector('.submenu--desktop'));
 }
 
 function toggleMenuIcon(){
@@ -43,7 +58,7 @@ function toggleMenuIcon(){
 
 function openMobileNav() {
     if(hamburgerIcon || closeIcon) {
-        console.log('icon clicked');
+        // console.log('icon clicked');
         if(heroNav.classList.contains('hero__nav-close') ) {
             heroNav.classList.remove('hero__nav-close');
         } else {
