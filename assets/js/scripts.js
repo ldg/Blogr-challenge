880fe85ffabaa4daa14ec2f1f4cb8076db6c7c2d
Scott McIntire <ldg@users.noreply.github.com>
2023-06-02 16:30:23 -0400

    added function to reset mobile nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-/* variables */
+/* ---variables--- */
 const logoContainer = document.querySelector('.hero__logo--container');
 const hamburgerIcon = document.querySelector('.hero__logo--container .hero--hamburger');
 const closeIcon = document.querySelector('.hero__logo--container .hero--close');
@@ -8,43 +8,21 @@ const desktopMenuLinks = document.querySelectorAll('ul.menu--desktop.menu-left l
 const mProduct = document.getElementById('mobile-product');
 const mCompany = document.getElementById('mobile-company');
 const mConnect = document.getElementById('mobile-connect');
-let width = window.matchMedia("(min-width: 600px)");
+const mSubmenus = document.querySelectorAll('li.menu__item ul.submenu');
+const mLinks = document.querySelectorAll('nav.hero__nav ul.menu-left li.menu__item > a');
 
-/* event listeners */
+
+/* ---event listeners--- */
 hamburgerIcon.addEventListener('click', toggleMenuIcon);
 closeIcon.addEventListener('click', toggleMenuIcon);
 mobileIconsContainer.addEventListener('click', openMobileNav);
 mProduct.addEventListener('click', openMobileSubmenu );
 mCompany.addEventListener('click', openMobileSubmenu );
 mConnect.addEventListener('click', openMobileSubmenu );
-// - https://fjolt.com/article/javascript-multiple-elements-addeventlistener
-// Array.prototype.forEach.call(desktopMenuLinks, function(item) {
-//     item.addEventListener('click', openDesktopMenu );
-// });
 
+/* ---functions--- */
 
-
-/* functions */
-function openMobileSubmenu(e) {
-    // console.log(e.target);
-    let submenuLinkTarget = this.querySelector('.submenu');
-    if(submenuLinkTarget.classList.contains('submenu-open')) {
-        submenuLinkTarget.classList.remove('submenu-open');
-    } else {
-        submenuLinkTarget.classList.add('submenu-open');
-    }
-
-}
-
-function openDesktopMenu(e) {
-    let linkTarget = this.querySelector('.submenu--desktop');
-    if(linkTarget.classList.contains('open') ){
-        linkTarget.classList.remove('open');
-    } else {
-        linkTarget.classList.add('open');
-    }
-}
-
+// toggles the hamburger menu icon when clicked
 function toggleMenuIcon(){
    if(hamburgerIcon.classList.contains('hide') ){
         hamburgerIcon.classList.toggle('hide');
@@ -54,8 +32,13 @@ function toggleMenuIcon(){
         closeIcon.classList.toggle('hide');
         hamburgerIcon.classList.toggle('hide');
    }
+   // reset all submenus when closing the mobile nav
+   closeAllSubMenus();
+   //resets active class when closing the mobile nav
+   removeActiveClassOnClose();
 }
 
+// toggles the mobile nav menu
 function openMobileNav() {
     if(hamburgerIcon || closeIcon) {
         // console.log('icon clicked');
@@ -69,10 +52,37 @@ function openMobileNav() {
     }
 }
 
-// window.addEventListener('resize', function() {
-//   if (window.innerWidth > 600) {
-//         heroNav.classList.remove("hero__nav-close");
-//     } else{
-//         heroNav.classList.add('hero__nav-close');
-//     }
-// });
+// toggles submenus on mobile nav
+function openMobileSubmenu(e) {
+    let submenuLinkTarget = this.querySelector('.submenu');
+    if(submenuLinkTarget.classList.contains('submenu-open')) {
+        submenuLinkTarget.classList.remove('submenu-open');
+        e.target.classList.remove('active');
+    } else {
+        submenuLinkTarget.classList.add('submenu-open');
+        e.target.classList.add('active');
+    }
+
+}
+
+// closes any open submenus when closing the mobile nav - called from toggleMenuIcon()
+function closeAllSubMenus() {
+    //convert nodelist to an array
+    mSubmenusArr = Array.from(mSubmenus);
+    //remove "submenu-open" class from all elements in the array, when closing the menu
+    mSubmenusArr.forEach(element => {
+       element.classList.remove('submenu-open'); 
+    });
+}
+
+//remove active class when mobile nav is closed
+function removeActiveClassOnClose(){
+    //convert nodelist to an array
+    mLinksArr = Array.from(mLinks);
+    // removes active class when mobile menu is closed
+    mLinksArr.forEach(element => {
+        element.classList.remove('active');
+    });
+}
+
+
